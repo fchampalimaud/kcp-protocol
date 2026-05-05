f3c760e49adeee5ec0212fc3fba7f4d21a255589
Luís Teixeira <micboucinha@gmail.com>
2026-05-05 14:34:32 +0100

    Update public API

--- src/kcp_protocol/__init__.py
+++ src/kcp_protocol/__init__.py
@@ -1,9 +1,8 @@
-from kcp_protocol.device import KCPFrame, KCPTransport
+from kcp_protocol.device import KCPTransport
 from kcp_protocol.devices.sauterfs220 import SauterFS220
 from kcp_protocol.transport import KCPSerialTransport
 
 __all__ = [
-    "KCPFrame",
     "KCPTransport",
     "KCPSerialTransport",
     "SauterFS220",
